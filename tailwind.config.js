51b5ecceb1970ce5b7f690a459718306693a1378
Mahmoud Selim <arafa@gmail.com>
2024-12-02 14:20:35 +0300

    dynamic routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem',
+        lg: '2rem',
+      },
+    },
     // extend: {
     //   colors: {
     //     background: "var(--background)",
@@ -13,5 +22,5 @@ module.exports = {
     //   },
     // },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
