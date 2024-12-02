5c03e6c651db722cc04d218791a46c93479ea027
Mahmoud Selim <arafa@gmail.com>
2024-12-02 11:27:36 +0300

    nav prettier-tailwaind

---
+++ app/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+import NavLink from '../ui/NavLink'
+
+const Header = () => {
+  return (
+    <header className="bg-gray-100 p-4">
+      <nav className="container">
+        <ul className="flex gap-3">
+          <li>
+            <NavLink href={'/'}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink href={'/about'}>About</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className="container">
+      <h1 className="mt-4 text-3xl font-bold">Next JS</h1>
+    </section>
+  )
+}
