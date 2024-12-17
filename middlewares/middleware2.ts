e070c91cc4365b5184982a23ecf3aec61ac76857
Mahmoud Selim <arafa@gmail.com>
2024-12-17 11:35:25 +0300

    middleware chain

---
+++ middlewares/middleware2.ts
@@ -0,0 +1,25 @@
+import {
+  NextResponse,
+  type NextFetchEvent,
+  type NextRequest,
+} from 'next/server'
+
+import { CustomMiddleware } from './chain'
+
+export function withMiddleware2(middleware: CustomMiddleware) {
+  return async (
+    request: NextRequest,
+    event: NextFetchEvent,
+    response: NextResponse,
+  ) => {
+    // Perform whatever logic the second middleware needs to do
+    const pathname = request.nextUrl.pathname
+    console.log('middleware2 =>', { pathname })
+
+    console.log('request cookies =>', request.cookies.get('middleware1'))
+    console.log('response cookies =>', response.cookies.get('vercel'))
+
+    // Call the next middleware and pass the request and response
+    return middleware(request, event, response)
+  }
+}
