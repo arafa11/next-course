6c863a5835c4b5119954542c3e02816a5e380401
Mahmoud Selim <arafa@gmail.com>
2024-12-19 12:09:37 +0300

    next auth

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { withMiddleware2 } from '@/middlewares/middleware2'
 
 export default chain([withMiddleware1, withMiddleware2])
 
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// }
+export const config = { matcher: ['/protected/:path*'] }
